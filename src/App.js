405cb41a10c861c04388eeb902827f3592878de0
kamikazizen <kamikazizen@gmail.com>
2023-06-29 17:33:06 +0300

    Button, Navbar and css-s

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
@@ -13,36 +14,13 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <BrowserRouter>
-          <div>
-            <Link className="App-link" to="/">Home</Link>
-            &nbsp;|&nbsp;
-            <Link className="App-link" to="/page2">Page2</Link>
-          </div>
-          <Switch>
-            <Route exact path="/">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-                <p>The current time is {currentTime}.</p>
-            </Route>
-            <Route path="/page2">
-                <p>This is page 2!</p>
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </header>
+    <div>
+	<Router>
+		<Navbar />
+	  	<Switch>
+	  		<Route path='/' exact />
+	  	</Switch>
+	</Router>
     </div>
   );
 }
